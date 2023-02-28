a8f340854ecc2081db78387919e0da5c0c735bab
liron vaknin <45284793+lironv@users.noreply.github.com>
2023-02-28 15:24:37 +0200

    Update binom.py

--- binom.py
+++ binom.py
@@ -10,7 +10,7 @@ def newton_binom(n):
         for j in range(i+1):
             coef = math.comb(i, j)
             if coef >= 1 and coef <= 9:
-                row.append('\033[90m{}\033[0m'.format(coef))
+                row.append('\033[0m'.format(coef))
             else:
                 row.append('\033[94m{}\033[0m'.format(coef))
         print(' '.join(row).center(n*4))
