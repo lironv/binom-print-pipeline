5761b7dd5319604900eb5bacc475a75298f15f24
liron vaknin <45284793+lironv@users.noreply.github.com>
2023-02-28 15:23:38 +0200

    Update binom.py

--- binom.py
+++ binom.py
@@ -8,9 +8,7 @@ def newton_binom(n):
     for i in range(n):
         row = []
         for j in range(i+1):
-            # Calculate the binomial coefficient using the math library
             coef = math.comb(i, j)
-            # Color the output gray if the coefficient is between 1 and 9, otherwise blue
             if coef >= 1 and coef <= 9:
                 row.append('\033[90m{}\033[0m'.format(coef))
             else:
