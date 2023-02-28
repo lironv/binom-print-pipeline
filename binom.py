2c52f5516bc70ab9ee7abd9bb61e8b125c6f766b
liron vaknin <45284793+lironv@users.noreply.github.com>
2023-02-28 15:11:42 +0200

    Update binom.py

--- binom.py
+++ binom.py
@@ -1,22 +1,20 @@
-from math import comb
-import sys 
+import sys
+import math
 
-
-def newton_binom(num):
-    try:
-        num = int(num)
-        if num < 0:
-            raise ValueError
-    except ValueError:
-        print("\033[91mInvalid input: Please provide a non-negative integer.\033[0m")
-        return
-
-    for i in range(num + 1):
-        if i <= 9:
-            print(f"\033[90m{i}: {comb(num, i):,}\033[0m")
-        else:
-            print(f"\033[34m{i}: {comb(num, i):,}\033[0m")
-            
 n = int(sys.argv[1])
-newton_binom(n)
 
+if n < 0:
+    print('\033[91mInvalid parameter: n should be a positive integer\033[0m')
+    sys.exit(1)
+
+def binom(n):
+    for i in range(n + 1):
+        if i in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+            print('\033[90m', end="")
+        else:
+            print('\033[94m', end="")
+        coef = math.comb(n, i)
+        print(coef, end=" ")
+    print('\033[0m')
+
+binom(n)
