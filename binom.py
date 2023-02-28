b10b2b7fa3253ad96aab9ef19d2d3bdf2f6aa4cb
liron vaknin <45284793+lironv@users.noreply.github.com>
2023-02-28 15:21:19 +0200

    Update binom.py

--- binom.py
+++ binom.py
@@ -3,18 +3,20 @@ import math
 
 n = int(sys.argv[1])
 
-if n < 0:
-    print('\033[91mInvalid parameter: n should be a positive integer\033[0m')
-    sys.exit(1)
 
-def binom(n):
-    for i in range(n + 1):
-        if i in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
-            print('\033[90m', end="")
-        else:
-            print('\033[94m', end="")
-        coef = math.comb(1, i)
-        print(coef, end=" ")
-    print('\033[0m')
+def newton_binom(n):
+    for i in range(n):
+        row = []
+        for j in range(i+1):
+            # Calculate the binomial coefficient using the math library
+            coef = math.comb(i, j)
+            # Color the output gray if the coefficient is between 1 and 9, otherwise blue
+            if coef >= 1 and coef <= 9:
+                row.append('\033[90m{}\033[0m'.format(coef))
+            else:
+                row.append('\033[94m{}\033[0m'.format(coef))
+        print(' '.join(row).center(n*4))
 
-binom(n)
+# Prompt the user for input and call the function
+n = int(input('Enter the number of rows for the Newton binomial triangle: '))
+newton_binom(n)
