8c2de6b85c2c53540eaaa31f72a842db5148df8f
liron vaknin <45284793+lironv@users.noreply.github.com>
2023-02-28 15:27:53 +0200

    Update binom.py

--- binom.py
+++ binom.py
@@ -1,6 +1,18 @@
 import sys
 import math
 
+Red = '\033[91m'
+Green = '\033[92m'
+Blue = '\033[94m'
+Cyan = '\033[96m'
+White = '\033[97m'
+Yellow = '\033[93m'
+Magenta = '\033[95m'
+Grey = '\033[90m'
+Black = '\033[90m'
+Default = '\033[99m'
+
+
 n = int(sys.argv[1])
 
 
@@ -10,9 +22,11 @@ def newton_binom(n):
         for j in range(i+1):
             coef = math.comb(i, j)
             if coef >= 1 and coef <= 9:
-                row.append('\033[0m'.format(coef))
+                row.append(Grey.format(coef))
             else:
-                row.append('\033[94m{}\033[0m'.format(coef))
+                row.append(Blue.format(coef))
         print(' '.join(row).center(n*4))
 
 newton_binom(n)
+
+
